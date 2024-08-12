51ed948c0db12c1ef1f75076afc21647d4b1135b
Antonio Ribeiro da Silva <107161034+silvatonyy@users.noreply.github.com>
2024-08-12 18:00:59 -0300

    Add files via upload

---
+++ exercicios.py
@@ -0,0 +1,42 @@
+# ATIVIDADE 1
+
+for i in range(5):
+    print("hello world")
+
+# ATIVIDADE 2
+
+for i in range(1, 151):
+    print(i)
+
+# ATIVIDADE 3
+
+for i in range(10, -1, -1):
+    print(i)
+
+# ATIVIDADE 4
+
+for i in range(2, 201, 2):
+    print(i)
+
+# ATIVIDADE 5
+
+for i in range(1, 11):
+    print(f"Tabuada do {i}:")
+    for j in range(1, 11):
+        print(f"{i} x {j} = {i * j}")
+    print()  
+
+# ATIVIDADE 6
+
+numero = int(input("Digite um número inteiro: "))
+
+for i in range(1, numero + 1):
+    if i % 2 != 0:
+        print(i)
+
+
+
+
+
+
+
